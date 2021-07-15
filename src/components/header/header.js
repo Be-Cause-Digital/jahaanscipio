fc5c05c91ad12b56490554ef2436f2714b70c94a
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-07-14 22:14:27 -0400

    Header functionality in place

---
+++ src/components/header/header.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import "./header.scss";
+
+const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
+  return (
+    <header>
+      <button
+        className={`hamburger hamburger--arrow ${navActive ? "is-active" : ""}`}
+        type="button"
+        onClick={toggleNav}
+      >
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+      </button>
+      <div className="desktop-heading">
+        <h2>
+          Jahaan Scipio<span className="position"> - Architect & Designer</span>
+        </h2>
+      </div>
+
+      <div className={`desktop-menu ${navActive ? "active" : ""}`}>
+        <nav>
+          <div className="title">
+            <h2>Jahaan Scipio</h2>
+            <h3>Architect & Designer</h3>
+          </div>
+          <div className="internal-links">
+            <a href="google.com">About</a>
+            <a href="google.com">Work</a>
+          </div>
+          <div className="external-links"></div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
